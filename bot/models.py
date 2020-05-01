d85b87ad17bde2d11ae9bc64fe5b86ebb0cb75f8
wityat <ttrttr449@gmail.com>
2020-05-01 20:33:11 +0300

    first parser

--- bot/models.py
+++ bot/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Tier(models.Model):
     name = models.CharField("Название", max_length=100)
@@ -8,9 +9,11 @@ class Tier(models.Model):
     def __str__(self):
         return f"{self.name}"
 
+
 class Tournament(models.Model):
     name = models.CharField("Название", max_length=100)
     tier = models.ForeignKey(Tier, on_delete=models.CASCADE, verbose_name="Статус")
+
 
 class Game(models.Model):
     team1 = models.ForeignKey('Team', related_name='team1', on_delete=models.CASCADE, verbose_name="Команда 1")
@@ -19,8 +22,13 @@ class Game(models.Model):
     team2_score = models.SmallIntegerField()
     tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE, verbose_name="Турнир")
 
+
 class Player(models.Model):
+    full_name = models.CharField("Имя фамилия", max_length=255)
+    nickname = models.CharField("Ник в игре", max_length=255)
+    country = models.CharField("Страна", max_length=100)
     team = models.ForeignKey('Team', related_name='players', on_delete=models.CASCADE, verbose_name="Команда")
+
 
 class Team(models.Model):
     name = models.CharField("Название", max_length=50)
