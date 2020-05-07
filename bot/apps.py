714e15f03f99b771bd01c4df16119e9b2139d9fa
wityat <ttrttr449@gmail.com>
2020-05-07 22:22:35 +0300

    fix bug with matches updating

--- bot/apps.py
+++ bot/apps.py
@@ -3,9 +3,4 @@ from django.apps import AppConfig
 
 class BotConfig(AppConfig):
     name = 'bot'
-    TOKEN = "1145428190:AAGYZlNYD7aZh6Bao0gWL5jqpG0YTU5OpOM"
-    ADMIN_ID = 338141569
-
-    TOKEN = "1145428190:AAGYZlNYD7aZh6Bao0gWL5jqpG0YTU5OpOM"
-    ADMIN_ID = 338141569
 
